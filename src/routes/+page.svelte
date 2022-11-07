b9ba4fb14457b036284258ac774ece21f3b42a3d
Bnyro <bnyro@tutanota.com>
2022-11-07 18:24:29 +0100

    nitter, new home page and some mobile fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,10 +7,51 @@
 	import 'uno.css';
 </script>
 
-<h1>Hello World!</h1>
+<section>
+	<h1>Welcome to kavin.rocks</h1>
+	<h2 id="cmd">> ~/<span>_</span></h2>
+	<div id="about">
+		<div>Hosted services can be found <a href="/services">here</a>.</div>
+		<div>The instances are spread over India, UK, US, Germany and France.</div>
+	</div>
+</section>
 
 <style>
 	h1 {
 		@apply text-4xl font-bold text-center;
+		margin-top: 15vh;
+	}
+
+	h2 {
+		@apply text-5xl font-bold text-center;
+	}
+
+	#cmd {
+		color: var(--accent-color);
+	}
+
+	#cmd>span {
+		font-weight: 900;
+		animation: infinite blinking 1s ease-in-out;
+	}
+
+	#about {
+		margin-top: 15vh;
+	}
+
+	#about div {
+		@apply text-sm font-bold;
+	}
+
+	@keyframes blinking {
+		0% {
+			opacity: 0;
+		}
+		50% {
+			opacity: 100;
+		}
+		100% {
+			opacity: 0;
+		}
 	}
 </style>
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -5,18 +5,6 @@
 
 <section>
 	<h2>Contact</h2>
-	<span>Email: <a href="mailto:kavin@kavin.rocks">kavin@kavin.rocks</a></span>
+	<p>Matrix: @kavin1337:matrix.org</p>
+	<p>Email: <a href="mailto:kavin@kavin.rocks">kavin@kavin.rocks</a></p>
 </section>
-
-<style>
-	section {
-		margin: 3rem;
-	}
-	a {
-		text-decoration: none;
-		color: var(--accent-color);
-	}
-	a:hover {
-		text-decoration: underline;
-	}
-</style>
